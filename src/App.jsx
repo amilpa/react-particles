14bbef024d2cf011fabd3aec83b0f894a1a321d4
amilpa <amilpa2020@gmail.com>
2023-11-09 22:25:56 +0530

    added firefly

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
+import { loadFireflyPreset } from "tsparticles-preset-firefly";
 import { useCallback } from "react";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
     // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+    await loadFireflyPreset(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
@@ -25,75 +26,85 @@ function App() {
         height="100vh"
         width="100vw"
         options={{
-          background: {
-            color: {
-              value: "#0d47a1",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
+          preset: "firefly",
         }}
       />
+      {/* <Particles */}
+      {/*   id="tsparticles" */}
+      {/*   init={particlesInit} */}
+      {/*   loaded={particlesLoaded} */}
+      {/*   height="100vh" */}
+      {/*   width="100vw" */}
+      {/*   options={{ */}
+      {/*     background: { */}
+      {/*       color: { */}
+      {/*         value: "#0d47a1", */}
+      {/*       }, */}
+      {/*     }, */}
+      {/*     fpsLimit: 120, */}
+      {/*     interactivity: { */}
+      {/*       events: { */}
+      {/*         onClick: { */}
+      {/*           enable: true, */}
+      {/*           mode: "push", */}
+      {/*         }, */}
+      {/*         onHover: { */}
+      {/*           enable: true, */}
+      {/*           mode: "repulse", */}
+      {/*         }, */}
+      {/*         resize: true, */}
+      {/*       }, */}
+      {/*       modes: { */}
+      {/*         push: { */}
+      {/*           quantity: 4, */}
+      {/*         }, */}
+      {/*         repulse: { */}
+      {/*           distance: 200, */}
+      {/*           duration: 0.4, */}
+      {/*         }, */}
+      {/*       }, */}
+      {/*     }, */}
+      {/*     particles: { */}
+      {/*       color: { */}
+      {/*         value: "#ffffff", */}
+      {/*       }, */}
+      {/*       links: { */}
+      {/*         color: "#ffffff", */}
+      {/*         distance: 150, */}
+      {/*         enable: true, */}
+      {/*         opacity: 0.5, */}
+      {/*         width: 1, */}
+      {/*       }, */}
+      {/*       move: { */}
+      {/*         direction: "none", */}
+      {/*         enable: true, */}
+      {/*         outModes: { */}
+      {/*           default: "bounce", */}
+      {/*         }, */}
+      {/*         random: false, */}
+      {/*         speed: 6, */}
+      {/*         straight: false, */}
+      {/*       }, */}
+      {/*       number: { */}
+      {/*         density: { */}
+      {/*           enable: true, */}
+      {/*           area: 800, */}
+      {/*         }, */}
+      {/*         value: 80, */}
+      {/*       }, */}
+      {/*       opacity: { */}
+      {/*         value: 0.5, */}
+      {/*       }, */}
+      {/*       shape: { */}
+      {/*         type: "circle", */}
+      {/*       }, */}
+      {/*       size: { */}
+      {/*         value: { min: 1, max: 5 }, */}
+      {/*       }, */}
+      {/*     }, */}
+      {/*     detectRetina: true, */}
+      {/*   }} */}
+      {/* /> */}
     </>
   );
 }
