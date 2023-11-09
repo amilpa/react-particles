335e5c38ec4460e20fd7663770ce7428673f44d2
amilpa <amilpa2020@gmail.com>
2023-11-09 22:35:45 +0530

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ import { useCallback } from "react";
 
 function App() {
   const particlesInit = useCallback(async (engine) => {
-    //console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFireflyPreset(engine);
   }, []);
 
